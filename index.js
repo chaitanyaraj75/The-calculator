22f314324c44d85b346a37c97c6b5d1fb550ca3d
Chaitanya Raj <rajchaitanya75@gmail.com>
2025-06-09 14:06:41 +0530

    Bugs fixed

--- index.js
+++ index.js
@@ -3,48 +3,50 @@ var noofbuttons = document.getElementsByClassName("keys").length;
 // alert(noofbuttons);
 // Variables used in whole script
 
-var input=0;
-var tempoutput=-1;
-var curroperator="";
-var dumboutput="0";
+var input = 0;
+var tempoutput = -1;
+var curroperator = "";
+var dumboutput = "0";
 // On Screen Pressed
 
 var noofbuttons = document.querySelectorAll(".keys").length;
 for (var i = 0; i < noofbuttons; i++) {
     // var butt=document.querySelectorAll(".keys")[i];
-    var key=document.querySelectorAll(".keys")[i]
-    key.addEventListener("click", function(){
+    var key = document.querySelectorAll(".keys")[i]
+    key.addEventListener("click", function () {
         // document.querySelector(".upscreen p").innerHTML=tempoutput+curroperator;
         // document.querySelector(".mainscreen h1").innerHTML="0";
         // alert("hi");
-        var let=this.innerHTML;
-        valueentry(let);
-        let=convert(let);
-        // alert(let);
-        clickanimation(let);
+        var index = this.innerHTML;
+        valueentry(index);
+        index = convert(index);
+        // alert(index);
+        clickanimation(index);
     })
 }
 
 //Keyboard pressed
 
-document.addEventListener("keydown",function (event) {
-    // alert(event.key);
-    var let=event.key;
-    // alert(event.key);
-    if(let=='Enter'){
-        let="=";
+document.addEventListener("keydown", function (event) {
+    var key = event.key;
+    var allowedkeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '+', '-', '*', '/', '%', '.',
+        'Enter', 'Backspace', 'Delete']
+
+    if(!allowedkeys.includes(key))return ;
+
+    if (key == 'Enter') {
+        key = "=";
     }
-    if(let=="Backspace"){
-        let="DE";
+    if (key == "Backspace") {
+        key = "DE";
     }
-    if(let=="Delete"){
-        let="AC";
+    if (key == "Delete") {
+        key = "AC";
     }
-    valueentry(let);
-    let=convert(let);
-    // alert(let);
-    pressanimation(let);
-    // valueentry(let);
+    valueentry(key);
+    key = convert(key);
+    pressanimation(key);
 })
 
 //Fuctions
@@ -52,7 +54,7 @@ document.addEventListener("keydown",function (event) {
 //When a button is pressed on keybord then this animation comes
 
 function pressanimation(i) {
-    var key=document.querySelector(".i"+i);
+    var key = document.querySelector(".i" + i);
     key.classList.add("pressed");
     // document.querySelector(".upscreen p").innerHTML=key;
     // document.querySelector(".mainscreen h1").innerHTML=".i"+i;
@@ -65,7 +67,7 @@ function pressanimation(i) {
 
 function clickanimation(i) {
     // alert(".i"+i);
-    var key=document.querySelector(".i"+ i);
+    var key = document.querySelector(".i" + i);
     key.classList.add("clicked");
     // document.querySelector(".upscreen p").innerHTML=key;
     // document.querySelector(".mainscreen h1").innerHTML=".i"+i;
@@ -75,134 +77,140 @@ function clickanimation(i) {
 }
 //To covert /,*,+,.,=,% into others to access
 
-function convert(let){
-    switch (let) {
+function convert(key) {
+    switch (key) {
         case '.':
-            let="a";
+            key = "a";
             break;
         case '%':
-            let="b";
+            key = "b";
             break;
         case '/':
-            let="c";
+            key = "c";
             break;
         case '*':
-            let="d";
+            key = "d";
             break;
         case '+':
-            let="e";
+            key = "e";
             break;
         case '=':
-            let="f";
+            key = "f";
             break;
         default:
             break;
     }
-    return let;
+    return key;
 }
 
 //Whenever a value in entered then this will run
 
-function valueentry(value){
-    if((value>='0'&&value<='9')||value=='.'||value=='00'){
-        dumboutput=0;
-        if(input=='0'){
-            if(value=='.'){
-                input=input+value;
+function valueentry(value) {
+    if ((value >= '0' && value <= '9') || value == '.' || value == '00') {
+        dumboutput = 0;
+        if (input == '0') {
+            if (value == '.') {
+                input = input + value;
             }
-            else{
-                input=value;
+            else {
+                input = value;
             }
         }
-        else{
-            input=input+value;
+        else {
+            input = input + value;
         }
-        document.querySelector(".mainscreen h1").innerHTML=input;
+        document.querySelector(".mainscreen h1").innerHTML = input;
     }
-    else if(value=='+'||value=='-'||value=='*'||value=='/'||value=='%'){
+    else if (value == '+' || value == '-' || value == '*' || value == '/' || value == '%') {
         // alert(dumboutput);
-        
-        if(tempoutput==-1){
-            if(dumboutput!=0){
-                tempoutput=dumboutput;
-                dumboutput=0;
+
+        if (tempoutput == -1) {
+            if (dumboutput != 0) {
+                tempoutput = dumboutput;
+                dumboutput = 0;
                 // alert(tempoutput);
             }
-            else{
-                tempoutput=input;
+            else {
+                tempoutput = input;
             }
-            curroperator=value;
-            input=0;
-            document.querySelector(".upscreen p").innerHTML=tempoutput+curroperator;
+            curroperator = value;
+            input = 0;
+            document.querySelector(".upscreen p").innerHTML = tempoutput + curroperator;
         }
-        else{
+        else {
             // alert(typeof (tempoutput));
-            var out=operation(tempoutput,input,curroperator);
-            curroperator=value;
-            tempoutput=out;
+            var out = operation(tempoutput, input, curroperator);
+            curroperator = value;
+            tempoutput = out;
             // alert(typeof(tempoutput))
-            input=0;
-            document.querySelector(".upscreen p").innerHTML=tempoutput+curroperator;
+            input = 0;
+            document.querySelector(".upscreen p").innerHTML = tempoutput + curroperator;
         }
-        document.querySelector(".mainscreen h1").innerHTML="0";
+        document.querySelector(".mainscreen h1").innerHTML = "0";
     }
-    else if(value=='='){
-        var out=operation(tempoutput,input,curroperator);
-        input=0;
-        tempoutput=-1;
-        curroperator="";
-        document.querySelector(".upscreen p").innerHTML="0";
-        document.querySelector(".mainscreen h1").innerHTML=out;
-        dumboutput=out;
+    else if (value == '=') {
+        var out = operation(tempoutput, input, curroperator);
+        input = 0;
+        tempoutput = -1;
+        curroperator = "";
+        document.querySelector(".upscreen p").innerHTML = "0";
+        document.querySelector(".mainscreen h1").innerHTML = out;
+        dumboutput = out;
     }
-    else if(value=='AC'){
-        input=0;
-        tempoutput=-1;
-        curroperator="";
-        document.querySelector(".upscreen p").innerHTML="0";
-        document.querySelector(".mainscreen h1").innerHTML="0";
+    else if (value == 'AC') {
+        input = 0;
+        tempoutput = -1;
+        curroperator = "";
+        document.querySelector(".upscreen p").innerHTML = "0";
+        document.querySelector(".mainscreen h1").innerHTML = "0";
     }
-    else if(value=='DE'){
-        var n=input.length;
-        if(n>1){
-            input=input.slice(0,n-1);
+    else if (value == 'DE') {
+        var n = input.length;
+        if (n > 1) {
+            input = input.slice(0, n - 1);
         }
-        else{
-            input=0;
+        else {
+            input = 0;
         }
-        document.querySelector(".mainscreen h1").innerHTML=input;
+        document.querySelector(".mainscreen h1").innerHTML = input;
     }
-    else{
-        document.querySelector(".upscreen p").innerHTML="0";
-        document.querySelector(".mainscreen h1").innerHTML="ERROR";
-        tempoutput=-1;
-        dumboutput=0;
-        curroperator="";
-        input=0;
+    else {
+        document.querySelector(".upscreen p").innerHTML = "0";
+        document.querySelector(".mainscreen h1").innerHTML = "ERROR";
+        tempoutput = -1;
+        dumboutput = 0;
+        curroperator = "";
+        input = 0;
     }
 }
 
 //These are for the operations to be performed
 
-function operation(first,second,operator){
-    var a=Number(first);
-    var b=Number(second);
-    if(operator=='+'){
-        return a+b;
+function operation(first, second, operator) {
+    var a = Number(first);
+    var b = Number(second);
+    if (operator == '+') {
+        return a + b;
     }
-    else if(operator=='-'){
-        return a-b;
+    else if (operator == '-') {
+        return a - b;
     }
-    else if(operator=='/'){
-        return a/b;
+    else if (operator == '/') {
+        if(b==0){
+            return "NOT DEFINED";
+        }
+        return a / b;
     }
-    else if(operator=='*'){
-        return a*b;
+    else if (operator == '*') {
+        return a * b;
     }
-    else if(operator=='%'){
-        return a%b;
+    else if (operator == '%') {
+        if(b==0){
+            return "NOT DEFINED";
+        }
+        return a % b;
     }
-    else{
-        return "ERROR";
+    else {
+        return b;
     }
 }
